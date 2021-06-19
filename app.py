373f1c3bfab4bac6385faf77479a33f7007cd020
Pranjal-Soni <thepranjalsoni@gmail.com>
2021-06-19 20:59:11 +0530

    logging functionality added to app.py file and config file added

--- app.py
+++ app.py
@@ -13,7 +13,10 @@ The file consists 2 important functions
 from fastapi import FastAPI,Request
 from fastapi.responses import HTMLResponse
 import joblib
+import logging
+import config
 import string
+import uvicorn
 import pandas as pd
 from sklearn.svm import SVC
 from fastapi.templating import Jinja2Templates
@@ -21,40 +24,55 @@ from pydantic import BaseModel
 
 import time
 
+
+logging.basicConfig(filename=config.LOGS_DIR,level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+
 #punctuations
 punc = string.punctuation
 #load the vectorizer
-vectorizer = joblib.load("./utils/vectorizer.pkl")
+vectorizer = joblib.load(config.VECTORIZER_PATH)
+logging.info("Vectorizer loaded Successfully")
+
 
 #load the model
-svc_classifier = joblib.load("./model/svc_classifier.pkl")
+svc_classifier = joblib.load(config.MODEL_PATH)
+logging.info("Model loaded Successfully")
 
 #html file templates
-templates = Jinja2Templates(directory="./templates/")
+templates = Jinja2Templates(directory=config.TEMPLATES_DIR)
 
 ### initialise application
-app = FastAPI(debug=True)
+app = FastAPI()
 
 @app.get("/")
 def home(request: Request):
     """Load Home page of the application"""
     return templates.TemplateResponse("index.html", {"request": request})
 
-#
+#variable to keep record of requests
 request_id  = 0
 
 @app.post("/predict_intent")    
 async def predict(text:str):
     """Preprocess the text and make prediction for the text input"""
+    
+    global request_id
+    request_id += 1
+
     #preprocess text
     input = ''.join([char for char in text if char not in punc]).lower()
     input = pd.DataFrame.sparse.from_spmatrix(vectorizer.transform([input]))
 
     #model prediction
     prediction = svc_classifier.predict(input.values)
-    global request_id
-    request_id += 1
+    
 
-    if prediction[0] == 0: return {"id":request_id,"intent":"FindConnection"}
-    else: return {"id":request_id,"intent":"DepartureTime"}
+    if prediction[0] == 0: result =  {"id":request_id,"intent":"FindConnection"}
+    else: result =  {"id":request_id,"intent":"DepartureTime"}
 
+    logging.info(f"Request ID : {request_id} TEXT : {text} INTENT : {result['intent']}")
+    return result
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ config.py
@@ -0,0 +1,4 @@
+MODEL_PATH = "./model/svc_classifier.pkl"
+VECTORIZER_PATH = "./utils/vectorizer.pkl"
+LOGS_DIR = "./logs/intentClassificationApp.log"
+TEMPLATES_DIR = "./templates/"
